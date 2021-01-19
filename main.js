bc3135b30929e296702484e1fa6022b20c287c6e
ev-horrosh <73499449+ev-horrosh@users.noreply.github.com>
2021-01-19 01:45:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+bird.addEventListener('click', function () {
+    bird.style.webkitAnimation = 'blink .5s steps(5)';
+
+});
+// bird.addEventListener('click', function () {
+//     bird.style.webkitAnimation = '';
+// });

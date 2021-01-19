9d4bbf9aed775a62453123e76a5b572e8c4301eb
ev-horrosh <73499449+ev-horrosh@users.noreply.github.com>
2021-01-19 02:19:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-bird.addEventListener('click', function () {
-    bird.style.webkitAnimation = 'blink .5s steps(5)';
+bird.addEventListener('click', ()=> {
+    bird.style.webkitAnimation = 'blink .5s steps(5) 1';
 
 });
 // bird.addEventListener('click', function () {
